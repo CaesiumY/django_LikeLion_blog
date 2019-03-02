aa376b688c6f1b47a696f8e57945e8bfdeaaaba7
caesiumy <mn0658@naver.com>
2019-03-02 22:32:20 +0900

    feat: Modify db from sqlite to postgresql

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'secondproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'likelion_blog',
+        'USER': 'postgres',
+        'PASSWORD': 'dbs2636',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
